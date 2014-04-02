df5b4e40e23d40f11f0f8091d1cecc442d2a725b
John C <johnkc1992@gmail.com>
2014-04-01 19:33:47 -0600

    Rename variables to be more descriptive. Add and edit comments. Remove some debug print calls.

--- searchEngine.py
+++ searchEngine.py
@@ -1,5 +1,8 @@
-# stopWords.py
+# searchEngine.py
 # adds stop words to a dictionary
+# parses 50 documents and indexes the words found in them
+# accepts a query string with limited boolean operations allowed
+# returns a list of documents that 
 
 import xml.etree.ElementTree as xml
 import re
@@ -13,15 +16,17 @@ with open('stopwords.txt', 'r') as wordList:
 
 class DOC:
 	# document frequency statically referenced
-	df = dict()
+	# each key maps to an array of DOC references that have that key
+	docsHave = dict()
 	
 	def __init__(self, doc):
+		self.index = int(doc.find("DOCNO").text.strip())
 		self.title = doc.find("TITLE").text.strip()
 		self.author = doc.find("AUTHOR").text.strip()
 		self.text = doc.find("TEXT").text.strip()
 		
 		# index words in document
-		self.wf = dict()
+		self.docHas = dict()
 		self.__index(self.title)
                 self.__index(self.author)
                 self.__index(self.text)
@@ -31,29 +36,27 @@ class DOC:
 		for word in re.sub('[^\w]+', ' ', str).split():
 			# don't index unnecessarily short words or numbers
 			if word not in stopWords and len(word) >= 4 and re.search('[a-zA-Z]', word):
-				if not word in DOC.df:
-					DOC.df[word] = 0
-				if not word in self.wf:
-					DOC.df[word] += 1
-					self.wf[word] = 1
+				if not word in DOC.docsHave:
+					DOC.docsHave[word] = []
+				if not word in self.docHas:
+					DOC.docsHave[word].append(self)
+					self.docHas[word] = 1
 				else:
-					self.wf[word] += 1
+					self.docHas[word] += 1
 	
 	def calcRank(self, words):
 		sum = 0
 		for word in words:
 			# if word is in self.wf it should also be in DOC.df unless something bizarre goes terribly wrong
-			if word in self.wf:
-				sum += self.wf[word] / float(DOC.df[word])
-				print 'word "{}" passed the test!\nwf: {} ~ df: {} ~ sum: {}\n'.format(word, self.wf[word], DOC.df[word], sum)
-			else:
-				print 'word "{}" did not pass the test...\n'.format(word)
+			if word in self.docHas:
+				sum += self.docHas[word] / float(len(DOC.docsHave[word]))
 		return sum
 
+# fill an array of DOC instances parsed from each file
 docs = []
 for i in range(1, 51):
 	str = "documents/cranfield00" + ("0" + `i` if i < 10 else `i`)
 	docs.append(DOC(xml.parse(str).getroot()))
 
-#make sure it all works
-print docs[0].calcRank(['study', 'lift'])
+# make sure it all works
+print DOC.docsHave["data"]
